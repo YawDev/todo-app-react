1d82ec2404d6a8075a545b05de218644d17675bc
YawDev <94807175+YawDev@users.noreply.github.com>
2025-04-23 07:41:39 -0400

    Sign up added

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,47 +1,160 @@
-import { Button } from "react-bootstrap";
+import { Button, Form, Alert } from "react-bootstrap";
+import { RegisterAPI } from "../utils/GoServiceAuth";
+import { useNavigate } from "react-router-dom";
+import logoImg from "../assets/react.svg";
+import "../styles/Login.css";
+import { useState, useEffect } from "react";
 import "../styles/SignUp.css";
 
 function RegistrationForm({ isLoggedIn }) {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const navigate = useNavigate();
+  const [newUsername, setNewUserName] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [IsValid, setIsValid] = useState(false);
+  const [usernameError, setUserNameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [apiMessage, setApiMessage] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  useEffect(() => {
+    setNewUserName("");
+    setNewPassword("");
+    setIsValid(false);
+    setUserNameError("");
+    setPasswordError("");
+    setApiMessage("");
+    setShowAlert(false);
+  }, []);
+
+  useEffect(() => {
+    if (apiMessage) {
+      setShowAlert(true);
+      const timer = setTimeout(() => {
+        setShowAlert(false);
+        setApiMessage("");
+      }, 4000);
+      return () => clearTimeout(timer);
+    }
+  }, [apiMessage]);
+
+  const usernameOnChange = (e) => {
+    const input = e.target.value;
+    setNewUserName(input);
+    if (!input.trim()) {
+      setUserNameError("Username is required");
+      setIsValid(false);
+    } else {
+      setUserNameError("");
+      setIsValid(true);
+    }
   };
 
-  if (isLoggedIn) return <div>Logged In.</div>;
-  return (
-    <div className="signup-container">
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <h2 className="signup-title">Sign Up</h2>
+  const passwordOnChange = (e) => {
+    const input = e.target.value;
+    setNewPassword(input);
+    if (!input.trim()) {
+      setPasswordError("Password is required");
+      setIsValid(false);
+    } else {
+      setPasswordError("");
+      setIsValid(true);
+    }
+  };
 
-        <div className="signup-form-group">
-          <label htmlFor="username" className="signup-label">
-            Username
-          </label>
-          <input
-            type="text"
-            className="signup-input"
-            id="username"
-            placeholder="Enter a username"
-          />
-        </div>
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await RegisterAPI({
+        Username: newUsername,
+        Password: newPassword,
+      });
+      const { status: httpStatus, message } = data;
+      if (httpStatus === 200) {
+        alert("Account successfully created");
+        navigate("/login");
+      } else if (httpStatus === 400) {
+        setApiMessage(message);
+      }
+    } catch (error) {
+      console.log("Login failed", error);
+      setApiMessage("We apologize, Internal Server Error.");
+    }
+  };
 
-        <div className="signup-form-group">
-          <label htmlFor="password" className="signup-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="signup-input"
-            id="password"
-            placeholder="Enter a password"
-          />
-          <div className="signup-hint">Secure password required.</div>
-        </div>
+  return isLoggedIn ? (
+    <div>You are logged in.</div>
+  ) : (
+    <>
+      <div className="signup-form-section">
+        <Form onSubmit={handleSignUp} className="signup-form">
+          {apiMessage && (
+            <Alert
+              variant="danger"
+              className={`fade-alert ${
+                !showAlert ? "hide" : ""
+              } w-100 text-center`}
+            >
+              {" "}
+              {apiMessage}
+            </Alert>
+          )}
+          <h4>
+            Register New Account <img src={logoImg} alt="logo" />
+          </h4>
+          <div className="signup-form-group">
+            <Form.Label>New Username</Form.Label>
 
-        <Button type="submit" variant="primary">
-          Register
-        </Button>
-      </form>
-    </div>
+            <Form.Control
+              type="text"
+              placeholder="Enter new username"
+              id="username"
+              onChange={usernameOnChange}
+            />
+            {usernameError && (
+              <div style={{ color: "red", fontSize: "15px" }}>
+                {usernameError}
+              </div>
+            )}
+          </div>
+          <div className="signup-form-group">
+            <Form.Label>New Password</Form.Label>
+
+            <Form.Control
+              type="password"
+              placeholder="Enter new password"
+              id="password"
+              onChange={passwordOnChange}
+            />
+            <div className="signup-hint">Secure password required.</div>
+
+            {passwordError && (
+              <div style={{ color: "red", fontSize: "15px" }}>
+                {passwordError}
+              </div>
+            )}
+          </div>
+          <div className="signup-form-group">
+            <Form.Label>Confirm Password</Form.Label>
+
+            <Form.Control
+              type="password"
+              placeholder="Re-enter new password"
+              id="passwordConfirmation"
+              // onChange={passwordOnChange}
+            />
+
+            {passwordError && (
+              <div style={{ color: "red", fontSize: "15px" }}>
+                {passwordError}
+              </div>
+            )}
+          </div>
+
+          <Button disabled={!IsValid} variant="primary" type="submit">
+            Register
+          </Button>
+        </Form>
+      </div>
+    </>
   );
 }
 
